db04b8d2bc6512d9cdfedd7e5c8289147d607082
darvishdarab <mali_dd@yahoo.com>
2020-10-06 22:00:59 -0400

    starter code for the midterm project

---
+++ midterm_project/project.c
@@ -0,0 +1,36 @@
+// <your name> and <your JHED>
+// __Add your name and JHED above__
+/*****************************************************************************
+ * Midterm Project - A program to run the image processing operations
+ * Note: for naming convention, we try to follow Google C++ style guide:
+ *       https://google.github.io/styleguide/cppguide.html
+ * It is not compulsory, but you are highly encouraged to follow a convention.
+ *
+ * Summary: This file implements a program for image processing operations.
+ *          Different operations take different input arguments. In general,
+ *            ./project <input> <output> <operation name> [operation params]
+ *          The program will return 0 and write an output file if successful.
+ *          Otherwise, the below error codes should be returned:
+ *            1: Wrong usage (i.e. mandatory arguments are not provided)
+ *            2: Input file I/O error
+ *            3: Output file I/O error
+ *            4: The Input file cannot be read as a PPM file
+ *            5: Unsupported image processing operations
+ *            6: Incorrect number of arguments for the specified operation
+ *            7: Invalid arguments for the specified operation
+ *            8: Other errors 
+ *****************************************************************************/
+#include "ppm_io.h" // PPM I/O header
+
+// TODO: include requried headers for your projects.  
+// We recommend to put your image processing operations in 
+//  img_processing.h for decleartions and
+//  img_processing.c for their defintions
+// Then you should include the below header:
+//#include "img_processing.h" // Image processing header
+
+int main(int argc, char **argv) {
+	// TODO: write your codes here
+  
+  return 0;
+}
